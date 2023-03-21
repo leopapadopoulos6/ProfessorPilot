a8a17a8b82c3888e74a00d3f49b988553aae7490
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2023-03-21 01:06:25 -0400

    auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,45 @@
-import './App.css';
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar.js';
-import { Home } from './Pages/Home.js';
-import { Courses } from './Pages/Courses.js';
-import { Professors } from './Pages/Professors.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { PageLoader } from "./Components/page-loader";
+import { AuthenticationGuard } from "./Components/authentication-guard";
+import { AdminPage } from "./Pages/Admin-Page";
+import { CallbackPage } from "./Pages/Callback-Page";
+import { HomePage } from "./Pages/Home-Page";
+import { NotFoundPage } from "./Pages/Not-Found-Page";
+import { ProfilePage } from "./Pages/Profile-Page";
+import { ProtectedPage } from "./Pages/Protected-Page";
+import { PublicPage } from "./Pages/Public-Page";
 
+export const App = () => {
+  const { isLoading } = useAuth0();
 
-function App() {
+  if (isLoading) {
+    return (
+      <div className="page-layout">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/Courses' exact component={Courses} />
-          <Route path='/Professors' exact component={Professors} />
-        </Routes>
-        <Home/>
-
-
-
-      </Router>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/profile"
+        element={<AuthenticationGuard component={ProfilePage} />}
+      />
+      <Route path="/public" element={<PublicPage />} />
+      <Route
+        path="/protected"
+        element={<AuthenticationGuard component={ProtectedPage} />}
+      />
+      <Route
+        path="/admin"
+        element={<AuthenticationGuard component={AdminPage} />}
+      />
+      <Route path="/callback" element={<CallbackPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
-}
-
-export default App;
+};
---
+++ client/src/Pages/Home-Page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Auth0Features } from "../Components/auth0-features";
+import { HeroBanner } from "../Components/hero-banner";
+import { PageLayout } from "../Components/page-layout";
+
+export const HomePage = () => (
+  <PageLayout>
+    <HeroBanner />
+    <Auth0Features />
+  </PageLayout>
+);
